efcc3fda2af5d91839973d5eddac4821fd2efa44
Alex <rozenbaum1988@gmail.com>
2022-10-18 13:43:24 +0300

    cameras

--- routes/cameras.js
+++ routes/cameras.js
@@ -8,18 +8,19 @@ router.get("/", async(req, res) => {
     let page = req.query.page || 1;
     let sort = req.query.sort || "_id";
     let reverse = req.query.reverse == "yes" ? -1 : 1;
-    try {
-        let data = await CameraModel.find({})
-            .limit(perPage)
-            .skip((page - 1) * perPage)
-            .sort({
-                [sort]: reverse
-            })
-        res.json(data);
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({ msg: "there error try again later", err })
-    }
+    // try {
+    //     let data = await CameraModel.find({})
+    //         .limit(perPage)
+    //         .skip((page - 1) * perPage)
+    //         .sort({
+    //             [sort]: reverse
+    //         })
+    //     res.json(data);
+    // } catch (err) {
+    //     console.log(err);
+    //     res.status(500).json({ msg: "there error try again later", err })
+    // }
+    res.json({ msg: "cameras work" })
 })
 router.get("/search", async(req, res) => {
     try {
